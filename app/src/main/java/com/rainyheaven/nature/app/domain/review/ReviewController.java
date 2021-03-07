9110368d8bd82137a0761eb640d5514e2627d58a
Gwan Seok Yi <gooreumgil47@gmail.com>
2021-03-07 13:30:04 +0900

    review save 작업 ã중

--- app/src/main/java/com/rainyheaven/nature/app/domain/review/ReviewController.java
+++ app/src/main/java/com/rainyheaven/nature/app/domain/review/ReviewController.java
@@ -1,6 +1,12 @@
 package com.rainyheaven.nature.app.domain.review;
 
+import com.rainyheaven.nature.core.domain.review.dto.app.ReviewSaveRequestDto;
+import com.rainyheaven.nature.core.domain.user.TokenUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ReviewController {
 
+    @PostMapping
+    public ResponseEntity save(@RequestBody ReviewSaveRequestDto reviewSaveRequestDto, @AuthenticationPrincipal TokenUser tokenUser) {
 
+        return null;
+
+    }
 
 }
---
+++ core/src/main/java/com/rainyheaven/nature/core/domain/review/dto/app/ReviewSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.rainyheaven.nature.core.domain.review.dto.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewSaveRequestDto {
+
+    private Long itemId;
+    private String content;
+    private Integer rating;
+    private List<MultipartFile> files = new ArrayList<>();
+
+
+}
