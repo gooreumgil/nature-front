7c4c04abfce1077ff61f8ed016c050e554f0a1c0
Gwan Seok Yi <gooreumgil47@gmail.com>
2021-03-14 21:16:48 +0900

    emailSender profile fix

--- core/src/main/java/com/rainyheaven/nature/core/common/ConsoleEmailSendService.java
+++ core/src/main/java/com/rainyheaven/nature/core/common/ConsoleEmailSendService.java
@@ -3,9 +3,11 @@ package com.rainyheaven.nature.core.common;
 import com.rainyheaven.nature.core.common.dto.EmailVerifyNumSendRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile({"test", "local"})
 @Slf4j
 @RequiredArgsConstructor
 public class ConsoleEmailSendService implements EmailSender {
