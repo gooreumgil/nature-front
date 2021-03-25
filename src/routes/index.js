3501c6d270f4070eee9ffe3f15947b077d5e417d
Gwan Seok Yi <gooreumgil47@gmail.com>
2021-03-25 12:32:06 +0900

    권한 관련 작업

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import VueRouter from "vue-router";
+import VueCookies from 'vue-cookies';
+
+
 import Index from "@/views/Index";
 import Login from "@/views/auth/Login";
 import SignUp from "@/views/auth/SignUp";
@@ -14,9 +17,49 @@ import OrderDetail from "@/views/me/OrderDetail";
 import Search from "@/views/item/Search";
 import PasswordFind from "@/views/auth/PasswordFind";
 import PasswordChangeByEmail from "@/views/auth/PasswordChangeByEmail";
+import AuthApi from "@/api/AuthApi";
 
 
 Vue.use(VueRouter);
+Vue.use(VueCookies);
+
+/**
+ * 로그인 페이지로
+ */
+function goLoginPage() {
+  router.replace("/login");
+}
+
+/**
+ * 로그인이 필요한 페이지에 접근시에 세션검증
+ */
+function redirectLoginPageIfTokenNotValid(to, from, next) {
+  const token = Vue.$cookies.get("token");
+  const TAG = "[redirectLoginIfTokenNotValid]";
+
+  const thenFunc = function(response) {
+    const {accessToken} = response.data;
+    if (accessToken) {
+      Vue.$cookies.set("token", accessToken, '10d');
+    }
+    next();
+  }
+
+  const catchFunc = function(e) {
+    console.log(e);
+    Vue.$cookies.remove("token");
+    goLoginPage();
+  }
+
+  if (token) {
+    AuthApi.authenticate(token)
+      .then(thenFunc.bind(this))
+      .catch(catchFunc.bind(this));
+  } else {
+    goLoginPage();
+  }
+
+}
 
 export const router = new VueRouter({
   mode: 'history',
@@ -63,7 +106,8 @@ export const router = new VueRouter({
     },
     {
       path: '/my-page',
-      component: MeIndex
+      component: MeIndex,
+      beforeEnter: redirectLoginPageIfTokenNotValid
     },
     {
       path: '/my-page/orders/:id',
@@ -71,7 +115,8 @@ export const router = new VueRouter({
     },
     {
       path: '/orders',
-      component: OrderIndex
+      component: OrderIndex,
+      beforeEnter: redirectLoginPageIfTokenNotValid
     },
     {
       path: '/orders/:id/complete',
