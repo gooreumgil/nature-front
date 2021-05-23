4500dacb538573fbdd684280faf97a37c1f91f32
Gwan Seok Yi <gooreumgil47@gmail.com>
2021-05-23 20:19:50 +0900

    restdocs 작업

--- app/src/test/java/com/rainyheaven/nature/app/domain/user/UserControllerTest.java
+++ app/src/test/java/com/rainyheaven/nature/app/domain/user/UserControllerTest.java
@@ -36,6 +36,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -109,8 +111,6 @@ class UserControllerTest {
                 .andExpect(content().string(containsString("phoneNum2")))
                 .andExpect(content().string(containsString("phoneNum3")))
                 .andDo(document("get_user",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
                         responseFields(
                                 fieldWithPath("id").description("id"),
                                 fieldWithPath("name").description("닉네임"),
@@ -152,8 +152,6 @@ class UserControllerTest {
                 .content(objectMapper.writeValueAsString(userSaveRequestDto)))
                 .andExpect(status().isOk())
                 .andDo(document("register_user",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
                         requestFields(
                                 fieldWithPath("email").description("이메일"),
                                 fieldWithPath("name").description("닉네임"),
@@ -583,8 +581,75 @@ class UserControllerTest {
         List<Item> items = itemFactory.listInit();
         orderFactory.save(user, items);
 
-        mvc.perform(get("/v1/users/orders").header("Authorization", tokenGenerator.getToken(user)))
-                .andExpect(status().isOk());
+        mvc.perform(get("/v1/users/orders")
+                .header("Authorization", tokenGenerator.getToken(user))
+                .param("page", "0")
+                .param("size", "5")
+                .param("sort", "createdDate"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("get_order_pages",
+                        requestParameters(
+                                parameterWithName("page").description("페이지"),
+                                parameterWithName("size").description("사이즈"),
+                                parameterWithName("sort").description("정렬")
+                        ),
+                        responseFields(
+                                fieldWithPath("content").description("content"),
+                                fieldWithPath("content.[].id").description("id"),
+                                fieldWithPath("content.[].finalDiscountPrice").description("할인 금액 합산"),
+                                fieldWithPath("content.[].finalPrice").description("최종 가격"),
+                                fieldWithPath("content.[].usedPoints").description("사용한 포인트"),
+                                fieldWithPath("content.[].savedPoints").description("적립된 포인트"),
+                                fieldWithPath("content.[].status").description("주문 상태"),
+                                fieldWithPath("content.[].orderAt").description("주문 날짜"),
+                                fieldWithPath("content.[].deliveryResponseDto.id").description("배송 id"),
+                                fieldWithPath("content.[].deliveryResponseDto.receiver").description("배송 받는 사람 이름"),
+                                fieldWithPath("content.[].deliveryResponseDto.deliveryPrice").description("배송비"),
+                                fieldWithPath("content.[].deliveryResponseDto.memo").description("배송 메모"),
+                                fieldWithPath("content.[].deliveryResponseDto.phoneNum1").description("핸드폰 번호 앞자리"),
+                                fieldWithPath("content.[].deliveryResponseDto.phoneNum2").description("핸드폰 번호 중간"),
+                                fieldWithPath("content.[].deliveryResponseDto.phoneNum3").description("핸드폰 번호 마지막자리"),
+                                fieldWithPath("content.[].deliveryResponseDto.mainAddress").description("메인 주소"),
+                                fieldWithPath("content.[].deliveryResponseDto.detailAddress").description("상세 주소"),
+                                fieldWithPath("content.[].deliveryResponseDto.zipCode").description("우편번호"),
+                                fieldWithPath("content.[].deliveryResponseDto.status").description("배송 상태"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].id").description("주문 상품 id"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].itemId").description("상품 id"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].itemNameKor").description("상품 한글 이름"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].itemNameEng").description("상품 영어 이름"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].itemPrice").description("상품 가격"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].itemDiscountPrice").description("상품 할인 금액"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].itemQuantity").description("구매한 상품 수량"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].mainImgPath").description("상품 이미지 path"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].orderedAt").description("주문 날짜"),
+                                fieldWithPath("content.[].orderItemResponseDtos.[].isLeaveReview").description("리뷰를 남겼는지 여부"),
+                                fieldWithPath("pageable").description("페이징"),
+                                fieldWithPath("pageable.sort").description("페이징 정렬"),
+                                fieldWithPath("pageable.sort.unsorted").description("unsorted"),
+                                fieldWithPath("pageable.sort.sorted").description("sorted"),
+                                fieldWithPath("pageable.sort.empty").description("empty"),
+                                fieldWithPath("pageable.offset").description("offset"),
+                                fieldWithPath("pageable.pageSize").description("pageSize"),
+                                fieldWithPath("pageable.pageNumber").description("pageNumber"),
+                                fieldWithPath("pageable.paged").description("paged"),
+                                fieldWithPath("pageable.unpaged").description("unpaged"),
+                                fieldWithPath("totalElements").description("총 order 개수"),
+                                fieldWithPath("totalPages").description("전체 페이지"),
+                                fieldWithPath("number").description("number"),
+                                fieldWithPath("size").description("size"),
+                                fieldWithPath("last").description("last"),
+                                fieldWithPath("sort").description("sort"),
+                                fieldWithPath("sort.unsorted").description("unsorted"),
+                                fieldWithPath("sort.sorted").description("sorted"),
+                                fieldWithPath("sort.empty").description("empty"),
+                                fieldWithPath("first").description("first"),
+                                fieldWithPath("numberOfElements").description("numberOfElements"),
+                                fieldWithPath("empty").description("empty")
+
+                        )
+
+                ));
 
         Page<Order> orderPage = orderFactory.findByUser(user);
         assertEquals(orderPage.getTotalElements(), 1);
