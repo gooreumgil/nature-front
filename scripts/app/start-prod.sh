a238b13d3687cb2e32f2ee5466dc1648175c452b
gooreumgil <gooreumgil47@gmail.com>
2023-12-09 15:21:15 +0900

    update

--- scripts/app/start-prod.sh
+++ scripts/app/start-prod.sh
@@ -3,9 +3,11 @@ port="8060"
 
 cd ../../
 rm ${jar}
+fuser -k ${port}/tcp
+
+chmod +x ./gradlew
 ./gradlew :core:clean
 ./gradlew :core:cleanQuerydslSourcesDir
 ./gradlew :app:bootJar
-fuser -k ${port}/tcp
 rm nohup.out
 java -jar -Dspring.profiles.active=prod ${jar}
